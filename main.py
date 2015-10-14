cbd88abd64dd2c930dd22e46a132167092e31bd9
admica <crazychickenhead@gmail.com>
2015-10-14 14:27:45 -0500

    connect

--- main.py
+++ main.py
@@ -16,43 +16,85 @@ Builder.load_file("kivy.kv")
 class RootWidget(FloatLayout):
 
     host = '192.168.1.100'
-    port = 2000
+    port = 8000
 
     def __init__(self, **kwargs):
         super(RootWidget, self).__init__(**kwargs)
-        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.settimeout(5)
 
+
+    def cb_label_host_text(self, *args):
+        # dont allow some chars
+        if args[0][-1] in [ '\r', '\n', '   ', ' ' ]:
+            self.ids.input_host.text = args[0][:-1]
+        else:
+            # set host
+            self.host = args[0]
+
+    def cb_label_port_text(self, *args):
+        # only allow digits
+        try:
+            int(args[0][-1])
+            # set port
+            self.port = int(args[0])
+        except ValueError:
+            self.ids.input_port.text = args[0][:-1]
+
+
+
+    def cb_button_connect_press(self, *args):
+        # set connecting label
+        self.ids.label_connecting.text = "Connecting to %s:%s" % (self.host, self.port)
+
+        # go to connecting screen
+        self.ids.screenmgr.current = "screen_connecting"
+
+        # start client thread
         self.t = Thread(target=self.t_client, args=())
         self.t.start()
 
-    def cb_label_host_text(self, *args):
-        print args
 
-    def cb_label_host_port(self, *args):
-        print args
+    def t_client(self):
+        try:
+            print "Connecting...", self.host, self.port
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.settimeout(5)
+            self.s.connect((self.host, self.port))
+
+            Clock.schedule_once(self.conn_yes, 0)
+
+            # stop thread
+            return False
+
+        except Exception as e:
+            # failed to connect
+            del self.s
+
+            Clock.schedule_once(self.conn_no, 0)
+
+        # stop thread
+        return False
+
+
+    def conn_no(self, *args):
+        # change setup heading
+        self.ids.label_heading.text = "Failed to connect. Try again?"
+
+        # change back to setup screen
+        self.ids.screenmgr.current = "screen_setup"
+
+
+    def conn_yes(self, *args):
+        # connected, go to main screen
+        self.ids.screenmgr.current = "screen_main"
+
 
     def cb_press(self, keys):
+        """user pushed button on the ctrl panel"""
         print keys
         try:
             self.s.send(keys)
         except Exception as e:
             print "Error <%s> trying to send [%s]" % (e, keys)
-
-
-    def t_client(self):
-        try:
-            print "Connecting..."
-            self.s.connect((self.host, self.port))
-
-            # connected
-            print "Connected"
-
-        except Exception as e:
-            print "Error <%s> connecting to %s:%s" % (e, self.host, self.port)
-
-        # stop thread
-        return False
 
 
 class MyApp(App):
